418cad6bd800ce161cfa59797e8215013642542c
SaidFariz <89094870+SaidFariz@users.noreply.github.com>
2022-11-21 19:58:38 +0700

    Menambahkan operator assignment

---
+++ Operator Assignments.py
@@ -0,0 +1,29 @@
+# Operasi Assignment
+
+a = 6
+a += 2
+print("nilai a += 2 adalah :", a)
+
+b = 6
+b -= 4
+print("nilai b-= 4 adalah :", b)
+
+c = 9
+c *= 7
+print("nilai dari c *= 7 adalah :", c)
+
+d = 6
+d /= 2
+print("nilai d /= 2 adalah :", d)
+
+e = 8
+e //= 3
+print("nilai e //= 3 adalah :", e)
+
+f = 9
+f **= 2
+print("nilai f **= 2 adalah : ", f)
+
+g = 7
+g %= 5
+print(" nilai g %= 5 adalah ", g)
